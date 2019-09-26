11fc1c2731f934c93146f22634af28bcd13ae0c2
Mirko Dzudzar <mirkodzudzar@gmail.com>
2019-09-26 16:23:36 +0200

    The application login system has been completed

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -16,6 +16,25 @@
         </form>
         <!-- /.input-group -->
     </div>
+    <!--Login-->
+    <div class="well">
+        <h4>Login</h4>
+        <form action="includes/login.php" method="POST">
+
+            <div class="form-group">
+                <input name="username" type="text" class="form-control" 
+                placeholder="Username">
+            </div>
+            <div class="input-group">
+                <input name="user_password" type="password" class="form-control" placeholder="Password">
+                <span class="input-group-btn">
+                    <button class="btn btn-primary" name="login_button" type="submit">Login</button>
+                </span>
+            </div>
+        
+        </form>
+        <!-- /.input-group -->
+    </div>
 
     <!-- Blog Categories Well -->
     <div class="well">
