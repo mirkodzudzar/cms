a6ca7eaacaa1c21969d2ee4d1774d5d6ae864e65
Mirko Dzudzar <mirkodzudzar@gmail.com>
2019-10-06 22:36:41 +0200

    Editing active nav links, adding logout button to front page

--- includes/navigation.php
+++ includes/navigation.php
@@ -21,8 +21,25 @@
                     {
                         $cat_id = $row['cat_id'];
                         $cat_title = $row['cat_title'];
+                        
+                        $category_class = '';
+                        
+                        $registration_class = '';
+                        
+                        $page_name = basename($_SERVER['PHP_SELF']);
+                        
+                        $registration = 'registration.php';
+                        
+                        if(isset($_GET['category']) && $_GET['category'] == $cat_id)
+                        {
+                            $category_class = 'active';
+                        }
+                        elseif($page_name == $registration)
+                        {
+                            $registration_class = 'active';
+                        }
 
-                        echo "<li><a href='category.php?category={$cat_id}'>{$cat_title}</a></li>";
+                        echo "<li class='{$category_class}'><a href='category.php?category={$cat_id}'>{$cat_title}</a></li>";
                     }
                 
                     if(isset($_SESSION['user_role']))
@@ -66,7 +83,7 @@
                     }
                     else
                     {
-                        echo "<li><a href='registration.php'>Registration</a></li>";
+                        echo "<li class='{$registration_class}'><a href='registration.php'>Registration</a></li>";
                     }
                 ?>
                 
--- includes/sidebar.php
+++ includes/sidebar.php
@@ -18,21 +18,30 @@
     </div>
     <!--Login-->
     <div class="well">
-        <h4>Login</h4>
-        <form action="includes/login.php" method="POST">
-
-            <div class="form-group">
-                <input name="username" type="text" class="form-control" 
-                placeholder="Username">
-            </div>
-            <div class="input-group">
-                <input name="user_password" type="password" class="form-control" placeholder="Password">
-                <span class="input-group-btn">
-                    <button class="btn btn-primary" name="login_button" type="submit">Login</button>
-                </span>
-            </div>
         
-        </form>
+        <?php if(isset($_SESSION['user_role'])): ?>
+            
+            <h4>Logged in as <?php echo $_SESSION['username']; ?></h4>
+            <a href="includes/logout.php" class="btn btn-primary">Logout</a>
+        <?php else: ?>
+            
+            <h4>Login</h4>
+            <form action="includes/login.php" method="POST">
+
+                <div class="form-group">
+                    <input name="username" type="text" class="form-control" 
+                    placeholder="Username">
+                </div>
+                <div class="input-group">
+                    <input name="user_password" type="password" class="form-control" placeholder="Password">
+                    <span class="input-group-btn">
+                        <button class="btn btn-primary" name="login_button" type="submit">Login</button>
+                    </span>
+                </div>
+
+            </form>
+            
+        <?php endif; ?>
         <!-- /.input-group -->
     </div>
 
